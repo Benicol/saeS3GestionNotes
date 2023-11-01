ff4a07c72d9d772d1d12161b471846a267d9b292
noahm <noahm@LAPTOP-48MA2VOJ.home>
2023-11-01 11:10:10 +0100

    Classe OutilReseau fini avec test

--- src/modele/OutilReseau.java
+++ src/modele/OutilReseau.java
@@ -1,16 +1,31 @@
-package modele;
-
 /**
  * OutilReseau.java                                                                                 21/10/2015
  * No copyright.
+ */
+
+package modele;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+/**
+ * @author noah.miquel, jodie.monterde, benjamin.nicol, ugo.schardt
  */
 public class OutilReseau {
     /**
      * Methode qui permet de recuperer l'adresse IP de la machine.
      * @return l'adresse IP de la machine.
      */
-    public String getIp() {
-        //TODO
-        return "adresse ip"; //STUB
+    public static String getIp() {
+        String ip;
+        ip ="";
+        try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return ip;
+		
     }
 }
