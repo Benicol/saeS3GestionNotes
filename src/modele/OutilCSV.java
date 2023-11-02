bc2d327da27c87aee002d877305470aa33a21e37
UgoSchardt <ugo.schardt@iut-rodez.fr>
2023-11-02 11:49:05 +0100

    Création des classes MissingDataException et TestOutilCSV Code des classes OutilCSV TODO : Régler les problèmes de TestOutilCSV et éventuellement rajouter du code dans MissingDataException

--- src/modele/OutilCSV.java
+++ src/modele/OutilCSV.java
@@ -1,18 +1,58 @@
 package modele;
 
+import modele.exceptions.MissingDataException;
+
 /**
  * OutilCSV.java                                                                                 21/10/2015
  * No copyright.
+ * @author noah.miquel, jodie.monterde, benjamin.nicol, ugo.schardt
  */
 public class OutilCSV {
     
-    public String formaterToCSV(String[][] donnees) {
-        //TODO
-        return "message"; //STUB
+    private final static int TAILLE_PAR_DEFAUT = 100;
+    
+    /** Prend en argument un tableau de données et le converti en une String au format CSV;
+     * @param donnees
+     * @return String donneesCSV
+     * @throws MissingDataException 
+     */
+    public static String formaterToCSV(String[][] donnees) throws MissingDataException {
+        if (donnees == null) {
+            throw new MissingDataException();
+        }
+        String donneesCSV = "";
+        for (String[] ligne : donnees) {
+            int i = 0;
+            for (String element : ligne) {
+                donneesCSV += element + ";";
+                if (i == ligne.length - 1) {
+                    donneesCSV += "\n;";
+                }
+                i++;
+            }
+        }
+        return donneesCSV;
     }
 
-    public String[][] formaterToDonnees(String donneesCSV) {
-        //TODO
-        return new String[0][0]; //STUB
+    /** Prend en argument une String au format CSV et la converti en un tableau de données.
+     * @param donneesCSV
+     * @return String[][] donnees
+     * @throws MissingDataException 
+     */
+    public static String[][] formaterToDonnees(String donneesCSV) throws MissingDataException {
+        if (donneesCSV == null || donneesCSV.equals("")) {
+            throw new MissingDataException();
+        }
+        String[][] donnees = new String[TAILLE_PAR_DEFAUT][TAILLE_PAR_DEFAUT];
+        String[] lignes = donneesCSV.split("\n;");
+        int i = 0;
+        for (String ligne : lignes) {
+            String[] elements = ligne.split(";");
+            for (int j = 0; j < elements.length; j++) {
+                donnees[i][j] = elements[j];
+            }
+            i++;
+        }
+        return donnees;
     }
 }
---
+++ src/modele/exceptions/MissingDataException.java
@@ -0,0 +1,16 @@
+/*
+ * MissingDataException.java                                  2 nov. 2023
+ * IUT Rodez, info1 2022-2023, ni copyright ni "copyleft"
+ */
+package modele.exceptions;
+
+/** TODO comment class responsibility (SRP)
+ * @author Ugo Schardt
+ *
+ */
+public class MissingDataException extends Throwable {
+
+    /** TODO comment field role (attribute, association role) */
+    private static final long serialVersionUID = 1L;
+
+}
