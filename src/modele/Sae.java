3c7ee1c1ba9143136ec0438259e532de95e6f9cf
UgoSchardt <ugo.schardt@iut-rodez.fr>
2023-11-02 16:53:26 +0100

    Classe Sae codée, testée et avec les javadocs

--- src/modele/Sae.java
+++ src/modele/Sae.java
@@ -1,28 +1,67 @@
+/*
+ * Sae.java                                  2 nov. 2023
+ * IUT Rodez, info1 2022-2023, ni copyright ni "copyleft"
+ */
 package modele;
 
+/** Classe objet permettant de représenter une Sae
+ * à partir d'un identifiant et d'un libelle,
+ * et permettant d'y associer une note.
+ * @author noah.miquel, jodie.monterde, benjamin.nicol, ugo.schardt
+ */
 public class Sae implements Enseignement{
     private String libelle;
     private String identifiant;
     private double note;
 
+    /** Constructeur de la classe Sae
+     * @param identifiant
+     * @param libelle
+     */
     public Sae(String identifiant, String libelle) {
+        if (libelle == null || libelle.equals("") || identifiant == null || identifiant.equals("")) {
+            throw new IllegalArgumentException();
+        }
         this.libelle = libelle;
         this.identifiant = identifiant;
     }
+    
+    /** Getter de l'attribut note
+     * @return note
+     */
     public double getNote() {
         return note;
     }
-    public void setNote() {
+    
+    /** Setter de l'attribut note
+     * @param note
+     */
+    public void setNote(double note) {
+        if (note < 0 || note > 20) {
+            throw new IllegalArgumentException();
+        }
         this.note = note;
     }
+    
+    /** Getter de l'attribut libelle
+     * @return libelle
+     */
     public String getLibelle() {
         return libelle;
     }
+    
+    /** Getter de l'attribut identifiant
+     * @return identifiant
+     */
     public String getIdentifiant() {
         return identifiant;
     }
+    
+    /** Renvoie la combinaison de l'identifiant et du libelle
+     * @return intitule
+     */
     public String creerIntitule() {
-        //TODO
-        return ""; //STUB
+        String intitule = this.identifiant + " : " + this.libelle;
+        return intitule;
     }
 }
