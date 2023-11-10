154bdccb22d5924c1a5152628e4d33c927fda8bc
benjamin <benjamin.nicol@iut-rodez.fr>
2023-11-10 15:35:01 +0100

    Finalisation vue et création contrôleur, et d'un faut main pour pouvoir ouvrir celle ci

---
+++ src/controleur/VueReinitialiserControleur.java
@@ -0,0 +1,67 @@
+package controleur;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+/** 
+ * Controleur de la vue vue.VueReinitialiser.fxml
+ * @author noah.miquel, jodie.monterde, benjamin.nicol, ugo.schardt
+ */
+public class VueReinitialiserControleur {
+    /**
+     * Methode appeler lors du clic sur le bouton "annuler"
+     */
+    @FXML
+    void annulerPresser(ActionEvent event) {
+
+    }
+    /**
+     * Methode appeler lors de l'entrée de la souris sur un bouton plein (bouton violet)
+     * afin de le rendre plus foncer
+     * Boutons utilisant cette méthode : 
+     * - Bouton "Supprimer toutes les données"
+     */
+    @FXML
+    void boutonPleinEntree(MouseEvent event) {
+
+    }
+    /**
+     * Methode appeler lors de la sortie de la souris d'un bouton plein (bouton violet)
+     * afin de le rééclaircir
+     * Boutons utilisant cette méthode : 
+     * - Bouton "Supprimer toutes les données"
+     */
+    @FXML
+    void boutonPleinSortie(MouseEvent event) {
+
+    }
+    /**
+     * Methode appeler lors de l'entrée de la souris sur un bouton vide (bouton blanc)
+     * afin de le rendre plus foncer
+     * Boutons utilisant cette méthode : 
+     * - Bouton "Annuler"
+     */
+    @FXML
+    void boutonVideEntree(MouseEvent event) {
+
+    }
+    /**
+     * Methode appeler lors de la sortie de la souris d'un bouton vide (bouton blanc)
+     * afin de le rééclaircir
+     * Boutons utilisant cette méthode : 
+     * - Bouton "Annuler"
+     */
+    @FXML
+    void boutonVideSortie(MouseEvent event) {
+
+    }
+    /**
+     * Methode appeler lors du clic sur le bouton "Supprimer toutes les données"
+     */
+    @FXML
+    void supprimerToutesLesDonneesPresser(ActionEvent event) {
+
+    }
+
+}
